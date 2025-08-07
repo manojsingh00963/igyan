a8edde9591ae9b831188822470e716021b5bf27a
manojsingh00963 <manojsingh00963@example.com>
2025-08-07 16:31:02 +0530

    backend, made just authentication route

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
+require('dotenv').config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+  })
+  .catch((err) => console.error('MongoDB connection failed:', err));
